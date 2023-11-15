21e9a7dab3a92c1d0c9fdf0eb06b3e7124e73bea
Brooklyn Midzinski <118629948+BrooklynMidzinski@users.noreply.github.com>
2023-11-15 14:42:17 -0700

    Update Current Age.sql

--- Current Age.sql
+++ Current Age.sql
@@ -1,3 +1,25 @@
+
+--ISSUE:
+--The HR department requests a report on the percentage of employees within specific age intervals to aid in understanding the company's generational workforce distribution.
+
+--You have been provided with a database containing an employees table with the following schema:
+
+--employee_id (integer): A unique identifier for each employee.
+--date_of_birth (date): The birth date of the employee
+--Your task is to write an SQL query that calculates the current age of each employee, categorizes these ages into intervals (18-20, 26-40, and above 40 years), and returns the percentage of employees within each interval. Round all percentages to two decimal places.
+
+--Requirements:
+
+--Compute the current age of each employee from their date_of_birth based on the current date.
+--Use the calculated ages to categorize employees into the following age brackets: 18-20 (inclusive), 26-40, and above 40. Ensure the age ranges include the entire span (e.g., up to but not including 21 years for the 18-20 bracket).
+--Calculate the percentage of the total workforce that falls within each age bracket, rounding the result to two decimal places. For age brackets with no employees, the query should display 0.0.
+--The query should output a table with two columns: age_interval and percent (of numeric datatype). The age_interval should be a string representation of the age range ('18-20', '26-40', '>40'), and percent should be the corresponding percentage of total employees, rounded to two decimal places.
+
+
+
+
+--My Solution:
+
 WITH age_range AS (
     SELECT '18-20' AS age_interval
     UNION
@@ -26,3 +48,19 @@ LEFT OUTER JOIN (
     GROUP BY age_interval
     ORDER BY age_interval
 ) AS needed_info ON age_range.age_interval = needed_info.age_interval;
+
+
+
+
+--Study Solution:
+--Done By: monadius
+
+with ages as (
+  select extract(year from age(date_of_birth)) as age from employees
+)
+(select '18-20' as age_interval, round(100 * (select count(*) from ages where age between 18 and 20) / (select count(*) from ages), 2) as percent
+union
+select '26-40' as age_interval, round(100 * (select count(*) from ages where age between 26 and 40) / (select count(*) from ages), 2) as percent
+union
+select '>40' as age_interval, round(100 * (select count(*) from ages where age > 40) / (select count(*) from ages), 2) as percent)
+order by 1
